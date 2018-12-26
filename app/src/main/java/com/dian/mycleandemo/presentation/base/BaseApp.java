0ed2e675e2303eb094c1183abc954dc38ddc6cfc
HJK <hejingkai77690@163.com>
2018-12-26 10:08:21 +0800

    .

--- app/src/main/java/com/dian/mycleandemo/presentation/base/BaseApp.java
+++ app/src/main/java/com/dian/mycleandemo/presentation/base/BaseApp.java
@@ -1,17 +1,12 @@
 package com.dian.mycleandemo.presentation.base;
 
-import android.app.Activity;
 import android.app.Application;
-import android.app.Application.ActivityLifecycleCallbacks;
 import android.content.Context;
-import android.os.Bundle;
-
-import com.dian.mycleandemo.presentation.util.L;
 
 /**
  * Created by Dian on 2018/9/19.
  */
-public abstract class BaseApp extends Application implements ActivityLifecycleCallbacks {
+public abstract class BaseApp extends Application {
 
     private static BaseApp INSTANCE;
     public static BaseApp getInstance() {
@@ -21,7 +16,6 @@ public abstract class BaseApp extends Application implements ActivityLifecycleCa
     @Override
     public void onCreate() {
         super.onCreate();
-        registerActivityLifecycleCallbacks(this);
         INSTANCE = this;
         init();
     }
@@ -32,39 +26,4 @@ public abstract class BaseApp extends Application implements ActivityLifecycleCa
         return INSTANCE.getApplicationContext();
     }
 
-
-    @Override
-    public void onActivityCreated(Activity activity, Bundle bundle) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivityStarted(Activity activity) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivityResumed(Activity activity) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivityPaused(Activity activity) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
-        L.v(activity.getClass().getSimpleName());
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-        L.v(activity.getClass().getSimpleName());
-    }
 }
